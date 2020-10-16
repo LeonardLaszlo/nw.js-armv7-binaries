7013193398d8a5ad1382fe2f308bab621287e442
Leonard Laszlo <laslaul@yahoo.com>
2020-10-16 18:27:02 +0200

    Prepare for automatic build.
    Signed-off-by: Leonard Laszlo <laslaul@yahoo.com>

---
+++ automatic-build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+# TODO install curl, python, docker
+DOCKER_CONTAINER="laslaul/nwjs-arm-build-env"
+
+# Get the active branch from the official repo.
+NWJS_BRANCH=$(curl -s -N https://api.github.com/repos/nwjs/nw.js | python -c 'import sys, json; print(json.load(sys.stdin)["default_branch"])')
+echo "NW.js active branch: $NWJS_BRANCH"
+# Check whether the container already exists on the machine.
+CONTAINER_ID=$(docker ps -aqf "ancestor=$DOCKER_CONTAINER")
+if [ -z "$CONTAINER_ID" ]
+then
+  echo "Did not find container: $DOCKER_CONTAINER"
+  echo "Pulling: $DOCKER_CONTAINER"
+  docker pull "$DOCKER_CONTAINER" || docker image build --build-arg NWJS_BRANCH="$NWJS_BRANCH" -t "$DOCKER_CONTAINER" .
+  docker run "$DOCKER_CONTAINER"
+  CONTAINER_ID=$(docker ps -aqf "ancestor=$DOCKER_CONTAINER")
+else
+  echo "Found container with id: $CONTAINER_ID"
+  docker run "$CONTAINER_ID"
+fi
+
+echo "Started successfully container with id: $CONTAINER_ID"
